80d783783407035f0379d03d286481378aa3f010
Andrew Cistola <andrewcistola@ufl.edu>
2019-06-28 19:40:19 -0400

    Add files via upload

---
+++ rf/rf_code_50jacs.R
@@ -0,0 +1,11 @@
+library(dplyr)
+
+setwd("C:/Users/drewc/Documents/healthy_neighborhoods")
+
+acs <- read.csv("acs/acs_data_rates.csv")
+flch <- read.csv("flcharts/flcharts_data_50rate.csv")
+
+join = inner_join(acs, flch, by = "Tract")
+
+write.csv(join, "C:/Users/drewc/Documents/healthy_neighborhoods/rf/rf_master_50acs.csv")
+
