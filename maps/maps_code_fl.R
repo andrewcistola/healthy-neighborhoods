1edf317310229d8e275c1cfcca316020b4ae004c
Andrew Cistola <andrewcistola@ufl.edu>
2019-06-29 03:40:16 -0400

    Update maps_code_fl.R

--- maps/maps_code_fl.R
+++ maps/maps_code_fl.R
@@ -4,15 +4,27 @@ setwd("C:/Users/drewc/Documents/healthy_neighborhoods")
 
 library(ggmap)
 library(ggplot2)
-library(maps)
+library(acs)
+library(tigris)
+library(leaflet)
 
 register_google(key = "AIzaSyA9EoExZadzR60Oy6xnL_nDJ8wgNksxKNI", write = TRUE)
+api.key.install(key = "f6d68b9d7fc73bc998b16776f37836fbd4f187f8")
 
-# Get FL Map
+# Get FL Census Tract Map
 
 hdat = read.csv("rf/rf_data_50acs.csv")
 
-fl = map_data("state", region = "florida")
+fl = tracts(state = "FL")
 
-ggplot() + geom_polygon(data = fl, aes(long, y = lat, group = group)) + coord_quickmap()
+plot(fl)
 
+## Combine with Health Outcome Data
+
+dm = read.csv("maps/maps_data_dmct.csv")
+
+fldm = geo_join(fl, dm, "GEOID", "GEOID")
+
+## Create Map
+
+leaflet() %>% addPolygons(data = fldm, fillColor = "blue")
