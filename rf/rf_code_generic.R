e85a212ac08dc8d99569ce66047ab19f548bec51
Andrew Cistola <andrewcistola@ufl.edu>
2019-06-29 01:33:54 -0400

    Create rf_code_generic.R

---
+++ rf/rf_code_generic.R
@@ -0,0 +1,53 @@
+## Code Prep
+
+setwd("C:/Users/drewc/Documents/...")
+
+library(plyr)
+library(dplyr)
+library(randomForest)
+library(MASS)
+
+## Join Datasets
+
+a <- read.csv("data1/data1_data_a.csv")
+b <- read.csv("data2/data2_data_b.csv")
+
+join = inner_join(a, b, by = "Geo.Id")
+
+write.csv(join, ".../rf/rf_data_ab.csv") #clean in excel
+
+## Random Forest
+
+rf = read.csv("rf/rf_data_ab.csv")
+
+rf$Geo.Id <- NULL
+
+rf = rf %>% mutate_if(is.factor, as.numeric)
+
+of = randomForest(formula = Variable ~ ., data = rf, ntree = 1000, importance=TRUE)
+
+rank = importance(of)
+
+write.csv(rank, ".../rf/rf_results_rank.csv") # clean, select cutoff, transpose in excel 
+
+## Bind Variables to Prep Model
+
+rank = read.csv("rf/rf_results_cutoff.csv")
+
+bind = rbind.fill(rank, rf)
+
+write.csv(bind, ".../rf/rf_results_bind.csv") #remove NA and clean in excel
+
+mod = read.csv("rf/rf_results_bind.csv")
+
+frmla = as.formula(paste("Variable ~ ", paste(colnames(rank), collapse=" + "), sep = ""))
+
+fit = lm(frmla, data=mod)
+
+## Stepwise backwards Fit
+
+back = stepAIC(fit, direction="backward")
+
+final = data.frame(summary(back)$coefficients)
+
+write.csv(final, file = ".../rf/rf_results_final.csv") #sort coefficients in excel
