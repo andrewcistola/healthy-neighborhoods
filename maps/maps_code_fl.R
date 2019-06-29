c139bdec7a4d9a07edaaca8e18e5c74ec6c915b9
Andrew Cistola <andrewcistola@ufl.edu>
2019-06-29 04:26:17 -0400

    Update maps_code_fl.R

--- maps/maps_code_fl.R
+++ maps/maps_code_fl.R
@@ -19,12 +19,37 @@ fl = tracts(state = "FL")
 
 plot(fl)
 
-## Combine with Health Outcome Data
+## Combine with Data
 
 dm = read.csv("maps/maps_data_dmct.csv")
 
 fldm = geo_join(fl, dm, "GEOID", "GEOID")
 
-## Create Map
+## Create Map for Health Outcome
 
-leaflet() %>% addPolygons(data = fldm, fillColor = "blue")
+pal <- colorNumeric("Greens", domain = c(0, 120))
+
+leaflet() %>% addPolygons(data = fldm, 
+                          fillColor = ~pal(fldm$Diabetes.Mellitus),
+                          fillOpacity = 0.7, 
+                          weight = 0.2, 
+                          smoothFactor = 0.2) %>%
+  addLegend(pal = pal, 
+            values = fldm$Diabetes.Mellitus, 
+            position = "bottomright", 
+            title = "Diabetes Deaths by Census Tract in Florida")
+
+## Create Map for Social Vairable
+
+pal2 <- colorNumeric("Blues", domain = c(0, 120))
+
+leaflet() %>% addPolygons(data = fldm, 
+                          fillColor = ~pal2(fldm$College.Educated),
+                          fillOpacity = 0.7, 
+                          weight = 0.2, 
+                          smoothFactor = 0.2) %>%
+  addLegend(pal = pal, 
+            values = fldm$Diabetes.Mellitus, 
+            position = "bottomright", 
+            title = "College Educated by Census Tract in Florida")
+
