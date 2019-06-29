e83203bd0cef78dbca1015c62e641082cdaaf584
Andrew Cistola <andrewcistola@ufl.edu>
2019-06-29 13:45:39 -0400

    Create maps_code_generic.R

---
+++ maps/maps_code_generic.R
@@ -0,0 +1,12 @@
+## Prep Code
+
+setwd("C:/Users/drewc/Documents/healthy_neighborhoods")
+
+library(ggmap)
+library(ggplot2)
+library(acs)
+library(tigris)
+library(leaflet)
+
+register_google(key = "AIzaSyA9EoExZadzR60Oy6xnL_nDJ8wgNksxKNI", write = TRUE)
+api.key.install(key = "f6d68b9d7fc73bc998b16776f37836fbd4f187f8")
