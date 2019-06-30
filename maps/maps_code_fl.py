84ab24f857764b24c61ba25306edb3eed59a26b0
Andrew Cistola <andrewcistola@ufl.edu>
2019-06-29 22:09:31 -0400

    Update maps_code_fl.py

--- maps/maps_code_fl.py
+++ maps/maps_code_fl.py
@@ -2,7 +2,7 @@
 
 import os
 import pandas as pnd
-import numpy as np
+import matplotlib.pyplot as plt
 import folium
 
 os.chdir("C:/Users/drewc/Documents/healthy_neighborhoods")
@@ -10,5 +10,4 @@ os.chdir("C:/Users/drewc/Documents/healthy_neighborhoods")
 # Get Map of FL
 
 map_fl = folium.Map(location = [51.5074, 0.1278], 
-                    tiles = "Stamen Toner",
                     zoom_start = 11)
