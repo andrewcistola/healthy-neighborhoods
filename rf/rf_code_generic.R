581ae13adfe4d63c51830df3dfcd0c929d55bbe8
Andrew Cistola <andrewcistola@ufl.edu>
2019-06-29 22:49:41 -0400

    Update rf_code_generic.R

--- rf/rf_code_generic.R
+++ rf/rf_code_generic.R
@@ -7,15 +7,6 @@ library(dplyr)
 library(randomForest)
 library(MASS)
 
-## Join Datasets
-
-a <- read.csv("data1/data1_data_a.csv")
-b <- read.csv("data2/data2_data_b.csv")
-
-join = inner_join(a, b, by = "Geo.Id")
-
-write.csv(join, ".../rf/rf_data_ab.csv") #clean in excel
-
 ## Random Forest
 
 rf = read.csv("rf/rf_data_ab.csv")
