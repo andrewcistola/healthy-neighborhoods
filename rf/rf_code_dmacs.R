3f47069dca754f758d06f8dabb3d2cf29dc8f8ff
Andrew Cistola <andrewcistola@ufl.edu>
2019-06-29 23:03:38 -0400

    Update rf_code_dmacs.R

--- rf/rf_code_dmacs.R
+++ rf/rf_code_dmacs.R
@@ -7,15 +7,6 @@ library(randomForest)
 library(MASS)
 library(reshape)
 
-## Join Datasets
-
-acs <- read.csv("acs/acs_data_rates.csv")
-flch <- read.csv("flcharts/flcharts_data_50rate.csv")
-
-join = inner_join(acs, flch, by = "Tract")
-
-write.csv(join, "C:/Users/drewc/Documents/healthy_neighborhoods/rf/rf_master_50acs.csv") #clean in excel and select variable
-
 ## Random Forest
 
 rf = read.csv("rf/rf_master_dmacs.csv")
