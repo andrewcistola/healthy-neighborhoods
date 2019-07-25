2cfd43fcfeec403f6e5091118b77160c88eff6b6
Andrew Cistola <andrewcistola@ufl.edu>
2019-07-25 16:15:28 -0400

    tool

--- acs/acs_code_drew.R
+++ acs/acs_code_drew.R
@@ -9,15 +9,15 @@ output: html_document
 library(tibble)
 library(dplyr)
 
-setwd("C:/Users/drewc/Documents/healthy_neighborhoods/acs")
+setwd("C:/Users/drewc/Downloads")
 
-Social <- read.csv("ACS_17_5YR_DP02_with_ann.csv")
-Economic <- read.csv("ACS_17_5YR_DP03_with_ann.csv")
-Housing <- read.csv("ACS_17_5YR_DP04_with_ann.csv")
-Demographic <- read.csv("ACS_17_5YR_DP05_with_ann.csv")
+Social <- read.csv("ACS_16_5YR_DP02_with_ann.csv")
+Economic <- read.csv("ACS_16_5YR_DP03_with_ann.csv")
+Housing <- read.csv("ACS_16_5YR_DP04_with_ann.csv")
+Demographic <- read.csv("ACS_16_5YR_DP05_with_ann.csv")
 
 se = inner_join(Social, Economic, by = "GEO.id")
 seh = inner_join(se, Housing, by = "GEO.id")
-ACS = inner_join(seh, Demographic, by = "GEO.id")
+acs = inner_join(seh, Demographic, by = "GEO.id")
 
-write.csv(ACS, "C:/Users/drewc/Documents/Data/acs_data_master.csv")
+write.csv(acs, "C:/Users/drewc/Documents/GitHub/stories/data/acs_data_raw.csv")
